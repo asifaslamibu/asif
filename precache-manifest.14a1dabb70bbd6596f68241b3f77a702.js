e67d8539f2e877b7645256f9d64df4316bd96d8b
samad-dev <73749383+asifibupoto@users.noreply.github.com>
2022-02-11 17:35:32 +0500

    Updates

---
+++ precache-manifest.14a1dabb70bbd6596f68241b3f77a702.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c98d451efa72257c4b5b029120f6f0",
+    "url": "/asif/index.html"
+  },
+  {
+    "revision": "13acd23ffebc5be4b8c4",
+    "url": "/asif/static/css/2.4334786b.chunk.css"
+  },
+  {
+    "revision": "4ba852363e9b73f0e1c9",
+    "url": "/asif/static/css/main.95d7a999.chunk.css"
+  },
+  {
+    "revision": "13acd23ffebc5be4b8c4",
+    "url": "/asif/static/js/2.0c13509b.chunk.js"
+  },
+  {
+    "revision": "4ba852363e9b73f0e1c9",
+    "url": "/asif/static/js/main.54df1458.chunk.js"
+  },
+  {
+    "revision": "f3805a21d8a3b5c8ef50",
+    "url": "/asif/static/js/runtime-main.f40294d9.js"
+  },
+  {
+    "revision": "692e25072627597f72c11402f1ba76fe",
+    "url": "/asif/static/media/cv.692e2507.png"
+  },
+  {
+    "revision": "e7f1956ed133c427808cff2c49a810f8",
+    "url": "/asif/static/media/cv1.e7f1956e.png"
+  },
+  {
+    "revision": "4c1cba5f7496cf94240aa8aa3d67f76a",
+    "url": "/asif/static/media/cv2.4c1cba5f.png"
+  },
+  {
+    "revision": "2da239b51f31c877b33bb3bf4e81c849",
+    "url": "/asif/static/media/cv3.2da239b5.png"
+  },
+  {
+    "revision": "fdf35f0af93d30d092cb284563dbb732",
+    "url": "/asif/static/media/cv4.fdf35f0a.png"
+  },
+  {
+    "revision": "e3fc8c5d421cbe174345f39efb30c8b2",
+    "url": "/asif/static/media/foad35.e3fc8c5d.png"
+  },
+  {
+    "revision": "0a04cd22a69442e31c3df8f8e1adff1b",
+    "url": "/asif/static/media/foad_logo.0a04cd22.jpg"
+  },
+  {
+    "revision": "c31b5069f157b82943809ab2ecc38ef8",
+    "url": "/asif/static/media/git.c31b5069.svg"
+  }
+]);
